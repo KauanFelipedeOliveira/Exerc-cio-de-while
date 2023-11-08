f36f7537294d8f8458c479364fc82bce3aafbdb7
Nokaute <128047270+KauanFelipedeOliveira@users.noreply.github.com>
2023-11-08 11:36:18 -0300

    Add files via upload

---
+++ Exercício de while/javascript.js
@@ -0,0 +1,13 @@
+function Soma_seq(){
+    var n = parseInt(numero.value);
+    var soma = n;
+
+    while(n <= 5){
+        soma = soma + (n + 1);
+        n = n + 1;
+    
+      alert(soma);{
+
+      }
+    }
+}
